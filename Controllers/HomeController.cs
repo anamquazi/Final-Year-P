7e396b830c159a0ee2d1b8ffa2bb5b6e27ab7fef
Anam Quazi <anamquazi@gmail.com>
2022-06-02 08:24:50 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Perfomans.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Perfomans.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "Admin, User, Teamlead, Head")]
+
+        public IActionResult Index()
+        {
+            string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+            if(role == null)
+            {
+                return RedirectToAction("Account", "Login");
+            }
+            if (role == "Admin")
+            {
+                return RedirectToAction("Index", "Parameters");
+            }
+            if(role == "Teamlead")
+            {
+              return RedirectToAction("Index", "Evaluations");
+            }
+            if(role == "Head")
+            {
+                return RedirectToAction("Index", "Evaluations");
+
+            }
+            if (role == "User")
+            {
+                return RedirectToAction("Index", "Chart");
+            }
+
+            else
+            {
+                return Content($"Work in Progress");
+
+            }
+        }
+    }
+}
