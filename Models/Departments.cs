37538575cad5c5634ed66b024392f8587f15cdc3
Anam Quazi <anamquazi@gmail.com>
2022-06-02 08:23:54 +0530

    Add files via upload

---
+++ Models/Departments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Perfomans.Models
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public List<DepartmentParameters> DepartmentParameters { get; set; }
+        public List<User> User { get; set; }
+        public List<Groups> Groups { get; set; }
+        public Departments()
+        {
+            User = new List<User>();
+            Groups = new List<Groups>();
+        }
+    }
+}
